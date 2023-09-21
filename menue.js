6b5a51b017ba0664c92d72e375a3d05406cfba33
Ujwal-007 <Ujwal-007>
2023-09-21 09:33:58 +0545

    added delay

--- menue.js
+++ menue.js
@@ -22,18 +22,18 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal ('.home-img',{delay:350});
-sr.reveal ('.home-text',{delay:350});
-sr.reveal ('.home-img',{delay:350});
-sr.reveal ('.container',{delay:350});
+sr.reveal ('.home-img',{delay:300});
+sr.reveal ('.home-text',{delay:300});
+sr.reveal ('.home-img',{delay:300});
+sr.reveal ('.container',{delay:300});
 
 sr.reveal ('.about-img',{});
-sr.reveal ('.about-text',{delay:350});
+sr.reveal ('.about-text',{delay:300});
 
 sr.reveal ('.middle-text',{});
-sr.reveal ('.row-btn,.shop-content',{delay:350});
+sr.reveal ('.row-btn,.shop-content',{delay:300});
 
-sr.reveal ('.review-content,contact',{delay:350});
+sr.reveal ('.review-content,contact',{delay:300});
 
 
 
