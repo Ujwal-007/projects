3664653a9fbedfb39568fe04bc83923a6cc35846
Ujwal-007 <Ujwal-007>
2023-09-23 10:10:23 +0545

    .

--- shop.js
+++ shop.js
@@ -1,3 +1,8 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll",function() {
+    header.classList.toggle("sticky",window.scrollY > 80);
+});
 const sr = ScrollReveal({
     origin: 'top',
     distance: '85px',
